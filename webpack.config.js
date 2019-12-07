440f3b90c9912f1c0a0a193190acdb34e6096a67
KEEN <kyh0927@live.co.kr>
2019-12-08 01:01:55 +0900

    setting #2

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.tsx",
+  output: {
+    filename: "main.js",
+    path: __dirname + "/dist"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
+    ]
+  },
+  devServer: {
+    contentBase: './public',
+    publicPath: '/dist',
+    open: true
+  }
+};
